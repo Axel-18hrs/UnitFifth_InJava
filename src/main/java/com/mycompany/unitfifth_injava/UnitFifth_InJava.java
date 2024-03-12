175c67a401f5d09a4b5eadd0237e63dce82da8a2
Axel-18hrs <Axel.18hrs@gmail.com>
2024-03-12 00:24:36 -0600

    Mensaje del commit

---
+++ src/main/java/com/mycompany/unitfifth_injava/UnitFifth_InJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unitfifth_injava;
+import formulario.App;
+/**
+ *
+ * @author axel1
+ */
+public class UnitFifth_InJava {
+
+    public static void main(String[] args) {
+        App miApp = new App();
+         // Hacer visible la instancia de la clase App
+        miApp.setVisible(true); 
+    }
+}
